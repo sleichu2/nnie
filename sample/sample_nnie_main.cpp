d5572963a00b222a7997feb66a167f656522cf32
yangfei <yangfei@insta360.com>
2020-01-19 19:55:52 +0800

    add yilov3

--- sample/sample_nnie_main.cpp
+++ sample/sample_nnie_main.cpp
@@ -59,14 +59,14 @@ void yolov3DetectDemo(const char *model_path, const char *image_path)
     /*yolov3的参数*/
     int img_width = 416;
     int img_height = 416;
-    int num_classes = 3;
+    int num_classes = 80;
     int kBoxPerCell = 3;
 
     int feature_index0 = 0;
     int feature_index1 = 1;
     int feature_index2 = 2;
 
-    float conf_threshold = 0.05;
+    float conf_threshold = 0.2;
     float nms_threshold = 0.5;
     int is_nms = 1;
 
@@ -75,9 +75,9 @@ void yolov3DetectDemo(const char *model_path, const char *image_path)
     std::vector<float> confidences;
 
     const std::vector<std::vector<cv::Size2f>> anchors = {
-        {{58, 188}, {119, 254}, {238, 326}},
-        {{19, 56}, {36, 98}, {87, 96}},
-        {{6, 12}, {13, 24}, {39, 37}}};
+        {{116, 90}, {156, 198}, {373, 326}},
+        {{30, 61}, {62, 45}, {59, 119}},
+        {{10, 13}, {16, 30}, {33, 23}}};
 
     parseYolov3Feature(img_width,
                        img_height,
@@ -116,7 +116,39 @@ void yolov3DetectDemo(const char *model_path, const char *image_path)
                        confidences);
 
     std::vector<int> indices;
-    char *cls_names[] = {"person", "car", "dog"};
+    /*print result, this sample has 81 classes:
+      class 0:background      class 1:person       class 2:bicycle         class 3:car            class 4:motorbike      class 5:aeroplane
+      class 6:bus             class 7:train        class 8:truck           class 9:boat           class 10:traffic light
+      class 11:fire hydrant   class 12:stop sign   class 13:parking meter  class 14:bench         class 15:bird
+      class 16:cat            class 17:dog         class 18:horse          class 19:sheep         class 20:cow
+      class 21:elephant       class 22:bear        class 23:zebra          class 24:giraffe       class 25:backpack
+      class 26:umbrella       class 27:handbag     class 28:tie            class 29:suitcase      class 30:frisbee
+      class 31:skis           class 32:snowboard   class 33:sports ball    class 34:kite          class 35:baseball bat
+      class 36:baseball glove class 37:skateboard  class 38:surfboard      class 39:tennis racket class 40bottle
+      class 41:wine glass     class 42:cup         class 43:fork           class 44:knife         class 45:spoon
+      class 46:bowl           class 47:banana      class 48:apple          class 49:sandwich      class 50orange
+      class 51:broccoli       class 52:carrot      class 53:hot dog        class 54:pizza         class 55:donut
+      class 56:cake           class 57:chair       class 58:sofa           class 59:pottedplant   class 60bed
+      class 61:diningtable    class 62:toilet      class 63:vmonitor       class 64:laptop        class 65:mouse
+      class 66:remote         class 67:keyboard    class 68:cell phone     class 69:microwave     class 70:oven
+      class 71:toaster        class 72:sink        class 73:refrigerator   class 74:book          class 75:clock
+      class 76:vase           class 77:scissors    class 78:teddy bear     class 79:hair drier    class 80:toothbrush*/
+    //char *cls_names[] = {"person", "car", "dog"};
+    char *cls_names[] = {"background", "person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+
+                         "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep",
+
+                         "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase",
+
+                         "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+
+                         "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "donut", "apple", "sandwich",
+
+                         "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant",
+
+                         "bed", "diningtable", "toilet", "vmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave",
+
+                         "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"};
     std::vector<ObjectDetection> detection_results;
 
     if (is_nms)
@@ -140,7 +172,7 @@ void yolov3DetectDemo(const char *model_path, const char *image_path)
         auto remap_box = RemapBoxOnSrc(cv::Rect2d(box), img_width, img_height);
         ObjectDetection object_detection;
         object_detection.box = remap_box;
-        object_detection.cls_id = ids[idx];
+        object_detection.cls_id = ids[idx] + 1;
         object_detection.confidence = confidences[idx];
         detection_results.push_back(std::move(object_detection));
 
@@ -179,11 +211,10 @@ int main(int argc, char *argv[])
     case '1':
     {
         const char *image_path = "./data/nnie_image/test/dog_bike_car.bgr";
-        const char *model_path = "./data/nnie_model/detection/inst_yolo3.wk";
+        const char *model_path = "./data/nnie_model/detection/inst_yolov3_cycle.wk";
         yolov3DetectDemo(model_path, image_path);
     }
     break;
-
 
     default:
     {
